883833e159c4a1b78b8ce13fbc3e05472db569cc
Eriel Cabrera <erielcabrera1234@gmail.com>
2025-02-05 10:46:13 -0500

    .\lora_serial_recieve.py lora py file to recieve data

---
+++ lora_serial_recieve.py
@@ -0,0 +1,13 @@
+import serial
+import pandas as pd
+
+arduino = serial.Serial(port=' ', baudrate = 115200, timeout = 0.1)
+
+arduino.open() # open serial port to listen in for any incoming data
+
+while True:
+    if arduino.in_waiting: # returns the number of bytes available to read in the serial ports recieve buffer
+        packet = arduino.readline()
+        print(packet.decode('utf-8'))
+        # removes extra newline
+        # print(packet.decode('utf-8').rstrip('\n'))
